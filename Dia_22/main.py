ee3b8f1fc95af0321f7f4a631250c8a9b27a37e2
Vilker Rainer <vilkerrainer1@gmail.com>
2025-12-02 18:40:04 -0300

    Dia 22 Concluido

---
+++ Dia_22/main.py
@@ -0,0 +1,12 @@
+# ============================================================== #
+# ---------------------Funções Anônimas e Map------------------- #
+# Use map() para converter uma lista de strings para maiúsculas. #
+# ============================================================== #
+
+lista = ['python', 'dias', 'meses', 'verbo']
+resultado = list(map(lambda x: x.upper(), lista))
+print(resultado)
+
+# ================================= #
+# Para rodar: python -m Dia_22.main #
+# ================================= #
--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Este repositório documenta esse desafio. Cada dia trará um novo problema, proj
 | 19 | Diferença entre datas formatada | ✅ |
 | 20 | Decorador de tempo de execução | ✅ |
 | 21 | Gerador de Fibonacci | ✅ |
-| 22 | Uso de `map()` em strings | ⬜ |
+| 22 | Uso de `map()` em strings | ✅ |
 | 23 | Produto com `reduce()` | ⬜ |
 | 24 | Regex para e-mails válidos | ⬜ |
 | 25 | Implementação de lista ligada | ⬜ |
